989407f0bfbd3f7c9b62004185a918c1d24dc71e
ming <450742103@qq.com>
2019-01-14 10:59:19 +0800

    版本提交

---
+++ Swift_1/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Swift_1
+//
+//  Created by ming on 2019/1/10.
+//  Copyright © 2019 ming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.creatView()
+    }
+    func creatView()
+    {
+        let name = "Hellow，world!"
+        print(name)
+     
+        let varA = 42
+        print(varA)
+        
+        let varB = 3.14566
+        print(varB)
+        
+        
+        
+    }
+
+}
+
